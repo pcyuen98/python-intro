f350eaae87fdf7f65848f21a804e4bcd70f15067
User <User@LAPTOP-7770>
2020-11-24 12:09:59 +0800

    Updated Indention

--- src/IfElse.py
+++ src/IfElse.py
@@ -3,7 +3,7 @@ b = 200
 
 # Does 1000 value more than 10,000,000
 if a > b:
-  print ("a is bigger") # If True print A
+    print ("a is bigger") # If True print A
   
-else: 
-  print ("b is bigger") # If False print B 
+else:
+    print ("b is bigger") # If False print B 
