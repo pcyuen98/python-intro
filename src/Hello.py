9515e4894832bcfcc0cd5bac03e2f2f75b7e7b43
User <User@LAPTOP-7770>
2020-11-28 15:40:45 +0800

    new test commit

--- src/Hello.py
+++ src/Hello.py
@@ -5,5 +5,5 @@
 # In case the Projct Explorer Missing, check out stackoverflow.com
 # https://stackoverflow.com/questions/13623015/how-to-show-the-project-explorer-window-in-eclipse
 
-print ("Hello, World!")
+print ("Hello, World! 2")
 
