bab3a129073d84aae5e5d2b4a5f624b5ef5f7dfc
User <User@LAPTOP-7770>
2020-11-29 18:35:40 +0800

    Updated with combination of value

--- src/json/Json.py
+++ src/json/Json.py
@@ -1,10 +1,12 @@
 import json
 
 def loadJson():
-    # some JSON:
-    x =  '{ "name":"John", "age":30, "city":"New York"}'
-    # parse x:
+# some JSON:
+    x = '{ "name":"John", "age":30, "city":"New York"}'
+# parse x:
     y = json.loads(x)
-    # the result is a Python dictionary:
-    print(y["age"])
-    return y["age"]
+# the result is a Python dictionary:
+    print(y["name"] + '---' + y["city"] + '---' + str(y["age"]) )
+    return y["name"] + '---' + y["city"] + '---' + str(y["age"])
+
+print(loadJson())
