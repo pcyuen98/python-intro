d2a56dec0fd70a50e34c34890546e2488557bb09
User <pcyuen98@gmail.com>
2021-02-04 20:38:34 +0800

    Updates

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,13 +1,5 @@
 # More samples - https://www.w3schools.com/python/python_dictionaries.asp
 
-# dictionary
-dictionary = {1: '480', 2: '480', 3: '400'}
-
-print ('dictionary -->', dictionary)
-
-print ('dictionary value at 1 -->', dictionary.get(3))
-
-
 # dictionary Value
 dictionaryValue = {'one': '11', 'two': '22', 'three': '33'}
 
@@ -21,4 +13,14 @@ for key in dictionaryValue :
     if key == 'one': print ('the key is one'  )
     
 for key in dictionaryValue :
-    if dictionaryValue[key] == '11': print ('the value is 11'  )    
+    if dictionaryValue[key] == '11': print ('the value is 11'  ) 
+    
+    
+# More examples
+
+# dictionary
+dictionary = {1: '11', 2: '22', 3: '33'}
+
+print ('dictionary -->', dictionary)
+
+print ('dictionary value at 1 -->', dictionary.get(3))       
