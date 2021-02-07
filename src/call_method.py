61b938d711315bc1733e5427df4c781e2255d31f
User <pcyuen98@gmail.com>
2021-02-07 23:03:47 +0800

    Call method Updates

---
+++ src/call_method.py
@@ -0,0 +1,3 @@
+from method_class import method_class
+
+print( "new method with class: " , method_class.printHelloWorld())
---
+++ src/method_class.py
@@ -0,0 +1,17 @@
+
+class method_class:
+    
+    @classmethod
+    def sparePart(self):
+        x = 1
+        y = 1
+        z = 1
+        a = x + y + z
+        return a;
+         
+    @classmethod    
+    def printHelloWorld(self):  
+        return "hello World from class"
+
+
+
