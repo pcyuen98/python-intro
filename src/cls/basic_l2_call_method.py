565db864f6dda70b9c525afb45985a3df64eaad7
User <pcyuen98@gmail.com>
2021-02-08 13:33:42 +0800

    Updates

---
+++ src/cls/basic_l2_call_method.py
@@ -0,0 +1,8 @@
+from cls.basic_method_class import MethodClass
+m = MethodClass(); # --> Obj variable
+
+print( "Calling sparepart without Object: " , MethodClass.sparePart())
+
+print( "Calling sparepart with Object: " , m.sparePart())
+
+print( "Calling without Object: " , MethodClass.noSelf())
