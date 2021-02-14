41ae2370fc115a1bd9c029022efd5775a2c1cd88
User <pcyuen98@gmail.com>
2021-02-14 17:13:50 +0800

    Updates

--- src/cls/basic_l3_call_method.py
+++ src/cls/basic_l3_call_method.py
@@ -7,3 +7,5 @@ m.anIntTest = 1;
 print( "m anIntTest: " , m.getAnIntTest())
 print( "m getAnIntTest   With  ClassMethod   : " , m.getAnIntTestWithClassMethod())
 print( "m getAnIntTest   With  StaticMethod  : " , m.getAnIntTestWithStaticMethod(1))
+
+del m
