02f6516b427940ccb27fd80c806a39b03d377efb
User <pcyuen98@gmail.com>
2021-02-16 16:23:29 +0800

    Updates

---
+++ src/cls/server/json_server_search.py
@@ -0,0 +1,16 @@
+from cls.cls_object.person import Person
+from cls.server.json_server_method import JSONServerClass
+class JSONServerSearch:
+    
+    @staticmethod
+    def searchName(user_input):
+        print('searching for-->' , user_input.name)
+        for person in JSONServerClass.getPerson():
+            personLocal = Person()
+            personLocal.__dict__ = person
+            print('personLocal name-->' , personLocal.name)
+            isMatch = personLocal.name == user_input.name
+            print('isMatch ? -->' , isMatch)
+            if isMatch:
+                return True
+        return False
