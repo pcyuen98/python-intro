d87416e275388469f1e1d18101db946173471d09
User <pcyuen98@gmail.com>
2021-02-28 14:48:02 +0800

    updates

--- src/cls/server_exercise/hint/json_server_get_post.py
+++ src/cls/server_exercise/hint/json_server_get_post.py
@@ -1,6 +1,6 @@
-# This is to simulate the "POST" method from PostMan
+# This is to simulate the "POST" API from PostMan
 from cls.server.json_server_method import JSONServerClass
 JSONServerClass.setPerson('{"name":"Kiruban","address":"Selangor"}')
 
-# This is to simulate the "GET" method from PostMan
+# This is to simulate the "GET" API from PostMan
 print('value inside memory------------->' , JSONServerClass.getPerson())
