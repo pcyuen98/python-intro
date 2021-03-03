f03491c0e39d27a3a1c291bcd87a909353af94a8
User <User@DESKTOP-J4AKJIF>
2021-03-03 22:58:53 +0800

    Get Address

--- src/cls/server/json_server.py
+++ src/cls/server/json_server.py
@@ -38,9 +38,11 @@ class JsonSearch(object):
 class JsonSearchAddress(object):
     
     def GET(self):
+        import web
         # Exercise: Complete the method below 
-        
+        user_input = web.input()
         # JSONServerSearch.searchAddress(None)
+        isPersonAddressExist = JSONServerSearch.searchAddress(user_input)
         
         return False
 
