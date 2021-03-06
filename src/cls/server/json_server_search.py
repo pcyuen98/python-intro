cd8b357eaccd0eab357b077f04dd7c6b691a7908
User <User@DESKTOP-J4AKJIF>
2021-03-06 19:59:50 +0800

    hi

--- src/cls/server/json_server_search.py
+++ src/cls/server/json_server_search.py
@@ -1,15 +1,15 @@
-from cls.cls_object.person import Person
+from cls.cls_object.person import Car
 from cls.server.json_server_method import JSONServerClass
 class JSONServerSearch:
     
     @staticmethod
     def searchName(user_input):
-        print('searching for-->' , user_input.name)
+        print('searching for-->' , user_input.brand)
         for person in JSONServerClass.getPerson():
-            personLocal = Person()
+            personLocal = Car()
             personLocal.__dict__ = person
-            print('personLocal name-->' , personLocal.name)
-            isMatch = personLocal.name == user_input.name
+            print('personLocal name-->' , personLocal.brand)
+            isMatch = personLocal.brand == user_input.brand
             print('isMatch ? -->' , isMatch)
             if isMatch:
                 return True
@@ -17,9 +17,9 @@ class JSONServerSearch:
     
     @staticmethod
     def searchAddress(user_input):
-        print('searching for-->' , user_input.address)
+        print('searching for-->' , user_input.model)
         #for person in JSONServerClass.getPerson():
-        #    personLocal = Person()
+        #    personLocal = Car()
         #    personLocal.__dict__ = person
         #    print('personLocal address-->' , personLocal.address)
         #    isMatch = personLocal.address == user_input.address
