c857cbbe0bc807587dbc46a6d306face60606793
ALEX Kai <ALEX Kai@Kai>
2021-04-03 14:44:21 +0800

    Updates

--- src/cls/cls_object/person.py
+++ src/cls/cls_object/person.py
@@ -2,3 +2,4 @@ class Person:
     
     name = 'A Name'
     address = 'An Address'
+    postcode = 'An postcode'
--- src/cls/server/json_server_search.py
+++ src/cls/server/json_server_search.py
@@ -1,5 +1,6 @@
 from cls.cls_object.person import Person
 from cls.server.json_server_method import JSONServerClass
+from pickle import TRUE
 
 class JSONServerSearch:
     
@@ -18,21 +19,48 @@ class JSONServerSearch:
     
     @staticmethod
     def searchAddress(address):
-        print('searching for address-->' , address)
-        # Complete this implementation here
-        # the logic is similar to searchName method above. 
-        # You may copy line 9 to 16 from above, paste here then modify it 
+        print('Input address-->' , address)
+        for person in JSONServerClass.getPerson():
+            personLocal = Person() # new object creation
+            personLocal.__dict__ = person # convert to json
+            print ('\n') 
+            print ('personLocal address -->', personLocal.address)
+            #isBlackPrint = personLocal.name == 'BLACKPINK'
+            #print ('isBlackPrint --->' , isBlackPrint)
+            
+            isMatching = personLocal.address == address
+            print ('isMatching --->' , isMatching)
+            
+            if (isMatching):
+                return True
         return False
-
+    
+    @staticmethod
+    def searchPostCode(address):
+        print('Input address-->' , address)
+        for person in JSONServerClass.getPerson():
+            personLocal = Person() # new object creation
+            personLocal.__dict__ = person # convert to json
+            print ('\n') 
+            print ('personLocal address -->', personLocal.address)
+            #isBlackPrint = personLocal.name == 'BLACKPINK'
+            #print ('isBlackPrint --->' , isBlackPrint)
+            
+            isMatching = personLocal.address == address
+            print ('isMatching --->' , isMatching)
+            
+            if (isMatching):
+                return True
+        return False
+            
 # Load the Memory for string value below
 # This is to skip the Server Process 
 
-JSONServerClass.setPerson('{"name":"Kiruban","address":"Selangor"}')
+JSONServerClass.setPerson('{"name":"Kiruban","address":"Selangor","postcode":"10000" }')
+
+JSONServerClass.setPerson('{"name":"BLACKPINK","address":"mumbai","postcode":"250"}')
+JSONServerClass.setPerson('{"name":"MAMAMOO","address":"SEOUL","postcode":"1999"}')
 
 print('\n')
-name = 'Kiruban'
-print(name , ' name Matching? --->' , JSONServerSearch.searchName(name))
-
-print('\n')
-address = 'Selangor'
-print(address , ' address Matching? --->' , JSONServerSearch.searchAddress(address))
+address = 'mumbai'
+print(address , ' address exist? --->' , JSONServerSearch.searchAddress(address))
