56aa4f449465c47c45df9bedffaeacdb2ec86b8f
User <pcyuen98@gmail.com>
2021-11-11 21:41:20 +0800

    Updates

--- src/hello.py
+++ src/hello.py
@@ -2,12 +2,10 @@
 
 # Today we have technical issues, network issues, 
 
-# In case the Projct Explorer Missing, check out stackoverflow.com
+# In case the Project Explorer Missing, check out stackoverflow.com
 # https://stackoverflow.com/questions/13623015/how-to-show-the-project-explorer-window-in-eclipse
 
 # More samples - https://www.w3schools.com/python/
-
-# 28 November. 3pm Class - Day 1 - We can use compiler to compile into PYC file that able to execute faster
 
 print ("Hello, World! \n")
 
