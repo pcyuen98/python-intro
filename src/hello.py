000f8a6cd3763499213f3b06744d5dec866f2559
User <User@DESKTOP-1AMKAG9>
2022-10-04 20:45:55 +0800

    Update text

--- src/hello.py
+++ src/hello.py
@@ -2,7 +2,7 @@
 
 # Today we have technical issues, network issues, 
 
-# In case the Project Explorer Missing, check out stackoverflow.com
+# In case the Project Explorer or the project Missing, check out stackoverflow.com
 # https://stackoverflow.com/questions/13623015/how-to-show-the-project-explorer-window-in-eclipse
 
 # More samples - https://www.w3schools.com/python/
