c692a71ba077080a1106c32d22358d2cb71b8927
pcyuen98 <pcyuen98@gmail.com>
2022-10-10 13:30:50 +0800

    update

---
+++ src/exercise/input/exercise_basic_input_1.py
@@ -0,0 +1,17 @@
+# Objective - To get used to basic input
+
+# What to do
+# ==========
+# 1. Input the food and print the price
+# 2. Print invalid choice if wrong input
+
+print ("Food 1. Mcdonald RM 10")
+print ("Food 2. Nasi Lemak RM 2")
+print ("Food 3. Mee Kosong RM 1")
+
+x = input("Enter your food choice here:") 
+
+print ('Your selected food is ' + x + ' and the total price is RM')  
+
+
+# Quiz: If the drink is selectable, how do you calculate the total price?
