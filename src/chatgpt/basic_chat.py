8dc717336dd7873c15f065cddfe841f2d6887c3c
pcyuen98 <pcyuen98@gmail.com>
2023-03-08 19:01:47 +0800

    chatgpt

---
+++ src/chatgpt/basic_chat.py
@@ -0,0 +1,19 @@
+# Assume student will have some knowledge of basic installation of python and basic usages
+# get your api key from tutuorial here https://medium.com/geekculture/a-simple-guide-to-chatgpt-api-with-python-c147985ae28
+
+import openai
+
+def askQUes(ques):
+    openai.api_key = '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'
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": ques }]
+        )
+    return completion.choices[0].message.content
+
+ques = ''
+
+while ques != 'quit':
+    # Ask the user for a name.
+    ques = input("Ask a question, or enter 'quit': ")
+    print(askQUes(ques))
